a132226f8aa9c68ba9dacfd7a5e2128ccb63cfc3
Philipp Kitzberger <coding@kitze.net>
2023-09-13 18:38:48 +0200

    Add ext_emconf.php (for TER)

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Usebouncer',
+	'description' => 'Mail address validation via usebouncer.com (for powermail)',
+	'category' => 'system',
+	'state' => 'stable',
+	'author' => 'Philipp Kitzberger',
+	'author_email' => 'typo3@kitze.net',
+	'author_company' => '',
+	'version' => '1.1.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '11.5.0-11.5.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+];
