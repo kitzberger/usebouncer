7cd9c366679ebfca90d57144e8851ed747a7becc
Philipp Kitzberger <coding@kitze.net>
2024-03-06 10:48:43 +0100

    FIX: empty mail is considered to be spam

--- Classes/Domain/Validator/UsebouncerValidator.php
+++ Classes/Domain/Validator/UsebouncerValidator.php
@@ -25,17 +25,19 @@ class UsebouncerValidator extends AbstractValidator
         if ($this->configuration['_enable'] ?? false) {
             foreach ($mail->getAnswers() as $answer) {
                 if ($answer->getField()->isSenderEmail()) {
-                    $senderEmail = $answer->getValue();
-                    $this->api = GeneralUtility::makeInstance(Api::class);
-                    if ($this->api->checkMail($senderEmail) === false) {
-                        $result->addError(new Error(
-                            LocalizationUtility::translate(
-                                'usebouncer-doesnt-like-mail-address',
-                                'usebouncer'
-                            ),
-                            1692805794,
-                            ['marker' => $answer->getField()->getMarker()]
-                        ));
+                    $senderEmail = trim($answer->getValue());
+                    if (!empty($senderEmail)) {
+                        $this->api = GeneralUtility::makeInstance(Api::class);
+                        if ($this->api->checkMail($senderEmail) === false) {
+                            $result->addError(new Error(
+                                LocalizationUtility::translate(
+                                    'usebouncer-doesnt-like-mail-address',
+                                    'usebouncer'
+                                ),
+                                1692805794,
+                                ['marker' => $answer->getField()->getMarker()]
+                            ));
+                        }
                     }
                 }
             }
