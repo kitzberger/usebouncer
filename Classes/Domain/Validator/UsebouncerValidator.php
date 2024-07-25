5562edb9b166364f78eedd4372326d88e50ce40b
Philipp Kitzberger <coding@kitze.net>
2024-07-25 17:37:16 +0200

    FIX: undeliverable mail won't produce validation error

--- Classes/Domain/Validator/UsebouncerValidator.php
+++ Classes/Domain/Validator/UsebouncerValidator.php
@@ -6,8 +6,6 @@ use In2code\Powermail\Domain\Validator\AbstractValidator;
 use In2code\Powermail\Utility\LocalizationUtility;
 use Kitzberger\Usebouncer\Service\Api;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Error\Error;
-use TYPO3\CMS\Extbase\Error\Result;
 
 class UsebouncerValidator extends AbstractValidator
 {
@@ -17,7 +15,6 @@ class UsebouncerValidator extends AbstractValidator
     public function isValid(mixed $value): void
     {
         $mail = $value;
-        $result = new Result();
 
         if ($this->configuration['_enable'] ?? false) {
             foreach ($mail->getAnswers() as $answer) {
@@ -26,14 +23,13 @@ class UsebouncerValidator extends AbstractValidator
                     if (!empty($senderEmail)) {
                         $this->api = GeneralUtility::makeInstance(Api::class);
                         if ($this->api->checkMail($senderEmail) === false) {
-                            $result->addError(new Error(
+                            $this->setErrorAndMessage(
+                                $answer->getField(),
                                 LocalizationUtility::translate(
                                     'usebouncer-doesnt-like-mail-address',
                                     'usebouncer'
-                                ),
-                                1692805794,
-                                ['marker' => $answer->getField()->getMarker()]
-                            ));
+                                )
+                            );
                         }
                     }
                 }
